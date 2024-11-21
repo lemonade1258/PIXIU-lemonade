0bec1e7d287c5236f3274ba17ca020a668c8e591
lemonade <1779716932@qq.com>
2024-11-21 22:58:58 +0800

    update project

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from transformers import BartTokenizer
-
-# 加载模型
-tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
-
-# 打印模型的缓存路径
-print(tokenizer.pretrained_vocab_files_map)  # 这将列出模型相关文件的路径
